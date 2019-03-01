9d59544b68e4d5226f9441d3662af15a95330238
PlatinumFT <platinum2183@gmail.com>
2019-03-01 20:26:26 +0000

    Added list admins command.

---
+++ commands/owner/listadmin.js
@@ -0,0 +1,28 @@
+let discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+    let query = client.db;
+
+    let res = await query(`select * from admins'`);
+
+    let _admins = res.length,
+        _user;
+    if (_admins == 0) _string = "There are no Admins. Assign one!"
+    for (i = 0; i < _admins; i++) {
+        _user = client.users.get(admins[i]);
+        _string+=`${user.username}#${user.discriminator}`
+    }
+
+    let _embed = new Discord.RichEmbed()
+    .setAuthor(`List of Admins`, client.user.avatarURL)
+    .setColor(client.settings.colour)
+    .setDescription(_string)
+
+    message.channel.send(_embed);   
+}
+
+exports.help = {
+    name: "listadmin",
+    description: "Lists admins.",
+    usage: "listadmin",
+}
